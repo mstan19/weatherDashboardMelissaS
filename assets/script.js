af5d7a78867a2ab436e51cf6510af88dd13fc13a
melissastan <melissastan91@gmail.com>
2022-08-11 01:17:09 -0400

    added the api url to js

--- assets/script.js
+++ assets/script.js
@@ -3,3 +3,10 @@ $("#currentDate").text(weekDay);
 
 var currentTime = moment().format("hh:mm");
 $("#currentTime").text(currentTime);
+
+var apiKey = "9ab552a07c2506fa3f4d0091eda74d54"
+
+var city = "atlanta";
+
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+console.log(fetch(queryURL));
