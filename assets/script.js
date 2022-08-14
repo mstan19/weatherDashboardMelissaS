62bed4d4e4bd02574adfbf281d3ae8de8a9d4aec
melissastan <melissastan91@gmail.com>
2022-08-14 01:03:55 -0400

    finsished the current forecast

--- assets/script.js
+++ assets/script.js
@@ -4,40 +4,70 @@ $("#currentDate").text(weekDay);
 var currentTime = moment().format("hh:mm");
 $("#currentTime").text(currentTime);
 
+var GeoCodeAPIkey ="21b2bc695e4bd14a87688373d821c77c"
 var apiKey = "9ab552a07c2506fa3f4d0091eda74d54"
 var city;
 var searchHistoryEl = document.querySelector('#searchHistory');
 var cityInputEl = document.querySelector('#inputCity');
 var cityListEl = document.querySelector('#cityList');
 var cities = [];
-// var currentWindEl = document.querySelector('#wind');
+var userCityEl = document.querySelector('#userCity');
+var currentWindEl = document.querySelector('#windC');
+var currentTempEl = document.querySelector('#tempC');
+var currentHumidityEl = document.querySelector('#humidityC');
+var WeatherCondition = document.querySelector('#weatherCondition');
+var currentUVindex = document.querySelector('#UVindexC');
+var fiveWindEl = document.querySelector('.wind');
+var fiveTempEl = document.querySelector('.temp');
+var fiveHumidityEl = document.querySelector('.humidity');
+
 //console.log(fetch(queryURL));
 
 $("#search").on("click", function(event){
     event.preventDefault();
-    // if (city) {
-    //     getCity(city);
-    // cityInputEl.value = '';
-    // } else {
-    //   alert('Please Enter A City');
-    // }
-
     var userInput = $(this).parent().children(".userInput").val();
 
-    getCity(userInput);
-
+    getCityStepOne(userInput);
+    //displayCurrentWeather(userInput);
 });
 
 
-var getCity = function (userInput) {
-    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + userInput + "&appid=" + apiKey;
+var getCityStepOne = function (userInput) {
+    var queryURLGeoCode = "https://api.openweathermap.org/geo/1.0/direct?q=" + userInput + "&limit=1&appid=" + GeoCodeAPIkey;
 
-    fetch(queryURL)
+    fetch(queryURLGeoCode)
         .then(function (response) {
             if (response.ok) {
                 response.json().then(function (data) {
-                    // displayCurrentWeather(data, city);
-                   // console.log(data);
+                    console.log(data);
+                    userCityEl.innerHTML = data[0].name;
+                    var lat = data[0].lat;
+                    var long = data[0].lon;
+                    // console.log(response);
+                    //console.log(data);
+                    getCityStepTwo(lat, long);
+                });
+            } else {
+                alert('Error: ' + response.statusText);
+            }
+        })
+        .catch(function (error) {
+            alert('Unable to connect to the weather GeoCode API ');
+    });
+};
+
+var getCityStepTwo = function (lat, long) {
+   
+    var queryURLfree = "https://api.openweathermap.org/data/3.0/onecall?lat=" + lat + "&lon=" + long + "&exclude=&appid=" + apiKey;
+    fetch(queryURLfree)
+        //console.log(lat, long);
+        .then(function (response) {
+            if (response.ok) {
+                response.json().then(function (data) {
+                    console.log(data);
+                    displayCurrentWeather(data);
+                    //console.log(response);
+                    console.log(data);
                 });
             } else {
                 alert('Error: ' + response.statusText);
@@ -47,39 +77,53 @@ var getCity = function (userInput) {
             alert('Unable to connect to the weather API');
     });
 
-    // displayCurrentWeather (city);
-    displaySeachHistory(userInput);
+    //displaySeachHistory(userInput);
 };
 
-// function displayCurrentWeather (city) {
-//     currentWindEl.innerHTML = "Wind Speed: " + response.data.wind.speed + " MPH";
-// }
+
+function displayCurrentWeather (data) {
+    currentWindEl.innerHTML = "Wind: " + data.current.wind_speed + " MPH";
+    var Ftemp = Math.round(((data.current.temp - 273.15) * 1.8 + 32));
+    currentTempEl.innerHTML = Ftemp + " degrees F";
+    currentHumidityEl.innerHTML = data.current.humidity + " %";
+
+    var UVindex = data.current.uvi
+    currentUVindex.innerHTML = UVindex;
+    if (UVindex <= 2) {
+        $("#UVindexC").css("background-color", "#90ee90");
+        
+      } else if (3 <= UVindex >= 5) {
+        $("#UVindexC").css("background-color", "#FEA95E");
+
+      } else { UVindex >= 6 
+        $("#UVindexC").css("background-color", "red");
+      }    
+
+    //console.log(userInput);
+    fiveDayWeather(data);
+}
+
+function fiveDayWeather (data) {
+    for (let i = 0; i < 5; i++) {
+        fiveWindEl.innerHTML = data.daily[i].wind_speed + " MPH";
+        var Ftemp = Math.round(((data.daily[i].temp.day - 273.15) * 1.8 + 32));
+        fiveTempEl.innerHTML = Ftemp + " deg F";
+        fiveHumidityEl.innerHTML = "Humidity: " + data.daily[i].humidity + " %";
+      }
+}
 
 //creating the array of lists and adding them to the list
-
 function displaySeachHistory(userInput) {
-    // cityListEl.innerHTML = "";
-    
-    //console.log(userInput);
-
-    
-   
     cities.push(userInput);       
     for (var i = 0; i < cities.length; i++) {
       var city = cities[i];
-  
       var li = document.createElement("li");
       li.textContent = city;
- 
       searchHistoryEl.appendChild(li);
-
     }
+
     localStorage.setItem("city", JSON.stringify(cities));
     var storedListofCities = JSON.parse(localStorage.getItem("city"));
-    
-
-//console.log(storedListofCities);
-
   }
  // var storedListofCities = JSON.parse(localStorage.getItem("city"));
  //$("#cityList").textContent = (storedListofCities);
